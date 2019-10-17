be6fe892395fa1551c68353134b71c29a989538a
Daniel Lenahardt <ckwdani@gmail.com>
2019-10-17 16:50:06 +0200

    initial commit

---
+++ lib/lobevent_backend.dart
@@ -0,0 +1,11 @@
+/// lobevent_backend
+///
+/// A Aqueduct web server.
+library lobevent_backend;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
